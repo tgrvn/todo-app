daf2238f081748603d0b83419aee5ad7a90c8df2
Tigran <levc.tgrvn.97@gmail.com>
2023-11-18 21:01:32 +0200

    initial

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-gray-900">
+        <Aside />
+
+        <div class="h-screen"></div>
+    </div>
+</template>
+
+<script setup>
+import Aside from "@/Shared/Aside.vue";
+</script>
